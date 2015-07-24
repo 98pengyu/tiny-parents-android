1fc7dfbba5052b2a2e64701abcc567fb63b0bb09
py <1085029622@qq.com>
2015-07-24 17:58:01 +0800

    else

--- app/src/main/java/com/cloudiya/app/tiny_parents_android/activity/feedBackActivity.java
+++ app/src/main/java/com/cloudiya/app/tiny_parents_android/activity/feedBackActivity.java
@@ -1,7 +1,9 @@
 package com.cloudiya.app.tiny_parents_android.activity;
 
+import android.app.Activity;
+
 /**
  * Created by py on 7/23/15.
  */
-public class feedBackActivity {
+public class feedBackActivity extends Activity{
 }
